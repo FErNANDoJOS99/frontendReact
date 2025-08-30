7ab725e6cc6fcbe466dc651f919b4a07bb7a9316
Rou <rou@ciencias.unam.mx>
2025-08-29 20:53:14 -0600

    feat: Home page now works. You can now create, edit and delete a list, and the whole front has a new design.

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import ListView from './components/ListView';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/list/:listId" element={<ListView />} />
           <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
