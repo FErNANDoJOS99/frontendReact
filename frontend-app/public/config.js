42cc32804f863c8e2d8358d3b0d1334d758d3ac9
josefer <josefer@kali2.com>
2025-12-06 14:01:11 -0600

    returning to setting with env variable cooked in build

--- frontend-app/public/config.js
+++ frontend-app/public/config.js
@@ -1,4 +1,5 @@
-window._env_ = {
-    REACT_APP_API_BASE_URL : "DEFAULT_VALUE"
-  };
+// Descomentar si voy a usar variables reactivas despues del build
+// window._env_ = {
+//     REACT_APP_API_BASE_URL : "DEFAULT_VALUE"
+//   };
   
