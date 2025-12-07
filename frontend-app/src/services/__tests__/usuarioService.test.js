f0bfeb00604fa454d729f3777cf927405b85db80
FErNANDoJOS99 <111204390+FErNANDoJOS99@users.noreply.github.com>
2025-12-06 18:05:25 -0600

    Update usuarioService.test.js

--- frontend-app/src/services/__tests__/usuarioService.test.js
+++ frontend-app/src/services/__tests__/usuarioService.test.js
@@ -6,7 +6,7 @@ global.fetch = jest.fn();
 
 // Base URL desde variable de entorno (con fallback para tests)
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api2';
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
 //  process.env.REACT_APP_API_BASE_URL ;
 
 describe('usuarioService', () => {
